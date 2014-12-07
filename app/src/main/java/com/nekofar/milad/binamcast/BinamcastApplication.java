be339a32f321cbb28576fb2a2a6cb2249f131ee0
Milad Nekofar <milad@nekofar.com>
2014-12-07 12:58:13 +0330

    added binamcast application as default application

---
+++ app/src/main/java/com/nekofar/milad/binamcast/BinamcastApplication.java
@@ -0,0 +1,12 @@
+package com.nekofar.milad.binamcast;
+
+import android.app.Application;
+
+public class BinamcastApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
