7ed362163bec3de93758c94d9590352e3981e9bd
Milad Nekofar <milad@nekofar.com>
2014-12-11 22:14:49 +0330

    added path to the cast model properties

--- app/src/main/java/com/nekofar/milad/binamcast/model/Cast.java
+++ app/src/main/java/com/nekofar/milad/binamcast/model/Cast.java
@@ -19,6 +19,7 @@ public class Cast extends RealmObject {
     private String link;
     private String text;
     private String file;
+    private String path;
     private String image;
     private Date date;
 
@@ -60,6 +61,14 @@ public class Cast extends RealmObject {
 
     public void setFile(String file) {
         this.file = file;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 
     public String getImage() {
