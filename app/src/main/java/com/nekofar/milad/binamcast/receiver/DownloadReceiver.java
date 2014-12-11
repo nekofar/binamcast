d9e168d6b02d2361469d3d725e1b9cc4276cb9eb
Milad Nekofar <milad@nekofar.com>
2014-12-11 22:14:11 +0330

    added some improvements to the download receiver

--- app/src/main/java/com/nekofar/milad/binamcast/receiver/DownloadReceiver.java
+++ app/src/main/java/com/nekofar/milad/binamcast/receiver/DownloadReceiver.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import com.nekofar.milad.binamcast.R;
 import com.nekofar.milad.binamcast.common.Binamcast;
 import com.nekofar.milad.binamcast.model.Cast;
-import com.squareup.otto.Bus;
 
 import javax.inject.Inject;
 
@@ -21,34 +20,37 @@ public class DownloadReceiver extends BroadcastReceiver {
     private static final String TAG = DownloadReceiver.class.getSimpleName();
 
     @Inject
-    protected Bus mBus;
-
-    @Inject
     protected Realm mRealm;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        //
+        // Injecting Dagger modules
         ((Binamcast) context.getApplicationContext())
                 .getObjectGraph().inject(this);
 
-        //
+        // Get access to the DownloadManager service
         DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
 
-        //
+        // Using
         Cursor cursor = manager.query(new DownloadManager.Query().setFilterById(downloadId));
         cursor.moveToFirst();
         if (!cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_DESCRIPTION)).equals(context.getString(R.string.app_name))) {
             Log.d(TAG, "Not our app!");
         }
         else if (DownloadManager.STATUS_SUCCESSFUL == cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))) {
+            // Find cast using it's url
             Cast cast = mRealm.where(Cast.class)
                     .equalTo("file", cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_URI)))
                     .findFirst();
 
-            Log.d(TAG, cast.getId());
+            // Set save path of file for cast
+            mRealm.beginTransaction();
+            cast.setPath(cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI)));
+            mRealm.commitTransaction();
+
+            Log.d(TAG, cast.getPath());
         }
 
     }
